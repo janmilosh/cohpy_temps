2aec2489ff1eaac6c1916385be2c729f9af9d78e
janmilosh <janmilosh@gmail.com>
2016-04-14 08:43:18 -0400

    Complete meeting day methods

--- data_handler.py
+++ data_handler.py
@@ -1,20 +1,44 @@
-import os
-import requests
+import calendar
 import json
+import os
 import pdb
+import requests
+
 
 WU_API_KEY = os.environ.get('WU_API_KEY')
 
+def _get_meeting_day(year, month):
+    if month == 11:
+        return None
 
-def _get_normal_meeting_day(year, month):
-    pass
+    mondays = _mondays_in_month(year, month)
+    
+    if month == 5:
+        return _get_may_meeting_day(mondays)
+    elif month == 12:
+        return _get_december_meeting_day(mondays)
+    else:
+        return _get_normal_meeting_day(mondays)
 
-def _get_may_meeting_day(year):
-    pass
+def _get_normal_meeting_day(mondays):
+    return mondays[-1]
 
-def _get_december_meeting_day(year):
-    pass
+def _get_may_meeting_day(mondays):
+    return mondays[-2]
 
+def _get_december_meeting_day(mondays):
+    if mondays[0] < 3:
+        return mondays[1]
+    else:
+        return mondays[0]
+
+def _mondays_in_month(year, month):
+    mondays = []
+    month_matrix = calendar.monthcalendar(year, month)
+    for week in month_matrix:
+        if week[0] > 0:
+            mondays.append(week[0])
+    return mondays
 
 
 def _get_columbus_historical_max_temp(year, month, day):
@@ -24,4 +48,5 @@ def _get_columbus_historical_max_temp(year, month, day):
 
 
 if __name__ == '__main__':
-    print _get_columbus_historical_max_temp('2006', '03', '18')
+    print _get_meeting_day(2016, 12)
+    # print _get_columbus_historical_max_temp('2006', '03', '18')
